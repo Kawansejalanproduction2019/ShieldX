7c8457c0644570ae7eaf5fb1110bd9166c275e13
RH71 <67680354+Kawansejalanproduction2019@users.noreply.github.com>
2026-04-02 03:00:36 +0700

    Update versioning and add dependencies in build.gradle.kts
    Updated version code and name for the application. Added additional dependencies for material icons and navigation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rhdevs.shieldx"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.2.0" // Versi naik!
     }
 
     buildFeatures {
@@ -41,5 +41,10 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended") // Ikon tambahan
+    
+    // TAMBAHKAN INI UNTUK NAVIGASI KOMPLEKS
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    
     compileOnly("de.robv.android.xposed:api:82")
 }
