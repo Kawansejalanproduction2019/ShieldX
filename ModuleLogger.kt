601bf280d7a6f6cec35031ffdc9c0f7c1dd5d6ce
RH71 <67680354+Kawansejalanproduction2019@users.noreply.github.com>
2026-05-18 22:44:19 +0700

    Delete ModuleLogger.kt

--- ModuleLogger.kt
+++
@@ -1,9 +0,0 @@
-package com.rhdevsx.adblockerlsp
-
-import de.robv.android.xposed.XposedBridge
-
-object ModuleLogger {
-    fun logBlock(appName: String, action: String) {
-        XposedBridge.log("AdBlockerLSP: [$appName] $action")
-    }
-}
