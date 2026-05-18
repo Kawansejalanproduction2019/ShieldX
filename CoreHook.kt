e5886f234d087c881d3cc5074da4b678855e1cbf
RH71 <67680354+Kawansejalanproduction2019@users.noreply.github.com>
2026-05-18 22:24:00 +0700

    Refactor scheme retrieval and update applySpoof call
    Refactor scheme retrieval to occur within the enabled check and update applySpoof method call to include lpparam.

--- CoreHook.kt
+++ CoreHook.kt
@@ -11,14 +11,17 @@ class CoreHook : IXposedHookLoadPackage {
         
         val targetApp = lpparam.packageName
         val isEnabled = prefs.getBoolean(targetApp, false)
-        val scheme1 = prefs.getBoolean("${targetApp}_scheme1", true)
-        val scheme2 = prefs.getBoolean("${targetApp}_scheme2", false)
-        val scheme3 = prefs.getBoolean("${targetApp}_scheme3", false)
 
         if (isEnabled) {
+            val scheme1 = prefs.getBoolean("${targetApp}_scheme1", true)
+            val scheme2 = prefs.getBoolean("${targetApp}_scheme2", false)
+            val scheme3 = prefs.getBoolean("${targetApp}_scheme3", false)
+
+            ModuleLogger.logBlock(targetApp, "Modul aktif untuk aplikasi ini")
+
             if (scheme1) AdBlocker.applyDnsHook(lpparam)
             if (scheme2) AdBlocker.applyViewHook(lpparam)
-            if (scheme3) SpoofPixel.applySpoof()
+            if (scheme3) SpoofPixel.applySpoof(lpparam)
         }
     }
 }
