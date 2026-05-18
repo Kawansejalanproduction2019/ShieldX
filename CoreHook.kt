99af27ef3ff0dc0fe7883c1adbfc216b69480ef9
RH71 <67680354+Kawansejalanproduction2019@users.noreply.github.com>
2026-05-18 21:54:30 +0700

    Create CoreHook.kt

---
+++ CoreHook.kt
@@ -0,0 +1,24 @@
+package com.rhdevsx.adblockerlsp
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+import de.robv.android.xposed.XSharedPreferences
+
+class CoreHook : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        val prefs = XSharedPreferences("com.rhdevsx.adblockerlsp", "config")
+        prefs.makeWorldReadable()
+        
+        val targetApp = lpparam.packageName
+        val isEnabled = prefs.getBoolean(targetApp, false)
+        val scheme1 = prefs.getBoolean("${targetApp}_scheme1", true)
+        val scheme2 = prefs.getBoolean("${targetApp}_scheme2", false)
+        val scheme3 = prefs.getBoolean("${targetApp}_scheme3", false)
+
+        if (isEnabled) {
+            if (scheme1) AdBlocker.applyDnsHook(lpparam)
+            if (scheme2) AdBlocker.applyViewHook(lpparam)
+            if (scheme3) SpoofPixel.applySpoof()
+        }
+    }
+}
